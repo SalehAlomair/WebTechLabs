848b544198c1d7c30dd8929a7852d05df36a228e
SalehAlomair <pixel@webmail.com>
2026-04-16 19:17:09 +0300

    Add Lab 7 Django model and ORM query pages

--- DjangoProjects/apps/bookmodule/urls.py
+++ DjangoProjects/apps/bookmodule/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name="books.index"),
     path('search', views.search, name='books.search'),
+    path('simple/query', views.simple_query, name='books.simple_query'),
+    path('complex/query', views.complex_query, name='books.complex_query'),
     path('list_books/', views.list_books, name="books.list_books"),
     path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
     path('aboutus/', views.aboutus, name="books.aboutus"),
