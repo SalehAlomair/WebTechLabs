b51a4c9b07301549ddb9bb0c1bdc88e651262568
SalehAlomair <pixel@webmail.com>
2026-04-19 17:11:33 +0300

    Restore /books/index2/<int:val1>/ route for lab URL compatibility

--- DjangoProjects/apps/bookmodule/urls.py
+++ DjangoProjects/apps/bookmodule/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="books.index"),
+    path('index2/<int:val1>/', views.index2, name='books.index2'),
     path('search', views.search, name='books.search'),
     path('simple/query', views.simple_query, name='books.simple_query'),
     path('complex/query', views.complex_query, name='books.complex_query'),
--- DjangoProjects/apps/bookmodule/views.py
+++ DjangoProjects/apps/bookmodule/views.py
@@ -8,6 +8,10 @@ def index(request):
 def list_books(request):
     return render(request, 'bookmodule/list_books.html')
 
+
+def index2(request, val1=0):
+    return HttpResponse("value1 = " + str(val1))
+
 def viewbook(request, bookId):
     return render(request, 'bookmodule/one_book.html')
 
